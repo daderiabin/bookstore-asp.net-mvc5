2753d02713496cec2cf10c5ee413a8e7c2feab94
daderiabin <danylo.deriabin@gmail.com>
2020-02-19 22:31:41 -0600

    Added Categories to the Dashboard.

--- ECommerceShop/Controllers/AdminController.cs
+++ ECommerceShop/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ECommerceShop.DAL;
+using ECommerceShop.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,17 @@ namespace ECommerceShop.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+
         public ActionResult Dashboard()
         {
             return View();
+        }
+
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allCategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDeleted == false).ToList();
+            return View(allCategories);
         }
     }
 }
