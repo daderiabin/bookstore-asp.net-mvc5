cc89e9b73a7bbecce3a0b7cde52332a3d72b82e8
daderiabin <danylo.deriabin@gmail.com>
2020-02-26 01:53:30 -0600

    Added Shopping Cart with Add/Remove functions.

--- ECommerceShop/Controllers/HomeController.cs
+++ ECommerceShop/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerceShop.Models.Home;
+using ECommerceShop.DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace ECommerceShop.Controllers
 {
     public class HomeController : Controller
     {
+        BookStoreDbEntities db = new BookStoreDbEntities();
+
         public ActionResult Index(string search, int? page)
         {
             int pageSize = 8; //products per page
@@ -19,6 +22,82 @@ namespace ECommerceShop.Controllers
 
             HomePageViewModel model = new HomePageViewModel();
             return View(model.CreateModel(search, pageSize, page));
+        }
+
+        public ActionResult AddToCart(int productId)
+        {
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = db.Tbl_Product.Find(productId);
+
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = db.Tbl_Product.Find(productId);
+
+                foreach (var item in cart)
+                {
+                    if (item.Product.ProductId == productId)
+                    {
+                        int currentQty = item.Quantity;
+
+                        cart.Remove(item);
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = currentQty + 1
+                        });
+
+                        break;
+                    }
+                    else
+                    {
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = 1
+                        });
+                        break;
+                    }
+                }
+
+                Session["cart"] = cart;
+            }
+            return Redirect("Index");
+        }
+
+        public ActionResult RemoveFromCart(int productId)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+
+            foreach (var item in cart)
+            {
+                if (item.Product.ProductId == productId)
+                {
+                    cart.Remove(item);
+                    break;
+                }
+            }
+
+            if(cart.Count() > 0)
+            {
+                Session["cart"] = cart;
+            }
+            else
+            {
+                Session["cart"] = null;
+            }
+            
+            return Redirect("Index");
         }
 
         public ActionResult About()
