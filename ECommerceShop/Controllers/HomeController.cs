6ea6d307fab2db9879f01a7df330dffb8be974fb
daderiabin <danylo.deriabin@gmail.com>
2020-02-26 00:41:42 -0600

    Added search by Product/Category name functionality. Implemented pagination. Added some real products.

--- ECommerceShop/Controllers/HomeController.cs
+++ ECommerceShop/Controllers/HomeController.cs
@@ -9,10 +9,16 @@ namespace ECommerceShop.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string search, int? page)
         {
+            int pageSize = 8; //products per page
+            if (search != null)
+            {
+                page = 1;
+            }
+
             HomePageViewModel model = new HomePageViewModel();
-            return View(model.CreateModel());
+            return View(model.CreateModel(search, pageSize, page));
         }
 
         public ActionResult About()
--- ECommerceShop/Models/Home/HomePageViewModel.cs
+++ ECommerceShop/Models/Home/HomePageViewModel.cs
@@ -4,18 +4,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
+using PagedList;
+using PagedList.Mvc;
 
 namespace ECommerceShop.Models.Home
 {
     public class HomePageViewModel
     {
         public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
-        public IEnumerable<Tbl_Product> productList { get; set; }
-        public HomePageViewModel CreateModel()
+        BookStoreDbEntities db = new BookStoreDbEntities();
+        public IPagedList<Tbl_Product> productList { get; set; }
+        public HomePageViewModel CreateModel(string search, int pageSize, int? page)
         {
+            IPagedList<Tbl_Product> data = db.Database.SqlQuery<Tbl_Product>("SELECT * from Tbl_Product P " +
+                                                                             "left join Tbl_Category C on p.CategoryId=c.CategoryId " +
+                                                                             "where " +
+                                                                             "P.ProductName LIKE CASE WHEN @search is not null then '%'+@search+'%' else P.ProductName end " +
+                                                                             "OR " +
+                                                                             "C.CategoryName LIKE CASE WHEN @search is not null then '%'+@search+'%' else C.CategoryName end", 
+                                                                             new SqlParameter("@search", search ?? (object)DBNull.Value)).ToList().ToPagedList(page ?? 1, pageSize);
+
             return new HomePageViewModel
             {
-                productList = _unitOfWork.GetRepositoryInstance<Tbl_Product>().GetAllRecords()
+                productList = data
             };
         }
     }
